c9fd4e8fe13bca7242ee4e059f2e47e122c78d9a
charliejeynes <31928749+charliejeynes@users.noreply.github.com>
2020-02-27 15:21:28 +0000

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 15:48:54 2019
+
+@author: charliejeynes
+"""
+
+
+#import numpy as np
+
+#num = int(input("Enter a number: "))
+#mod = num % 2
+#if mod > 0:
+#    print("This is an odd number.")
+#else:
+#    print("This is an even number.")
+    
+l = list(range(101)) 
+
+for i in l :
+    theValue = i % 2
+    if theValue > 0:
+        print("odd")
+    else:
+        print("even")
+    
+    
