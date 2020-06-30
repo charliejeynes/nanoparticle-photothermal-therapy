2772d0f92cf8d931071d1078aaaa01fe129e4443
charlie jeynes <charliejeynes@charlies-MacBook-Pro.local>
2020-06-30 15:52:51 +0100

    added resolution output images

--- test_arc.m
+++ test_arc.m
@@ -18,10 +18,11 @@ directory = '/Users/charliejeynes/Projects/dia/sim_data/grid_res_test/';
 file_names = get_file_paths(directory);
 number_of_files = length(file_names); 
 
-
+bounds = 20 * 1e-3;  % this is the bounds of the world in m^3
 sum_absorption_list = []; 
 grid_resolution_lst = [];
 lines_profiles = cell(number_of_files, 1); 
+twoD_slice_lst = cell(number_of_files, 1); 
 
 for i = 1:number_of_files
     
@@ -29,9 +30,10 @@ for i = 1:number_of_files
     
     datacube = read_nc(file);
     twoD_slice = get_twoD_slice(datacube);
+    twoD_slice_lst{i, 1} = twoD_slice; 
     grid_resolution = get_grid_resolution(twoD_slice); 
     grid_resolution_lst(i) = get_grid_resolution(twoD_slice);
-    sum_absorption_list(i) = sum_output_datacube(twoD_slice, grid_resolution); 
+    sum_absorption_list(i) = sum_output_datacube(twoD_slice, bounds, grid_resolution); 
     line_profile = get_line_profile(twoD_slice, grid_resolution); 
     lines_profiles{i, 1} = line_profile; 
     
@@ -39,7 +41,12 @@ for i = 1:number_of_files
 end
 
 figure, 
-semilogy(sum_absorption_list)
+plot(grid_resolution_lst, sum_absorption_list, 'x')
+xlabel('resolution X^2)')
+ylabel('sum absorption * cell area [W/m^2)') 
+
+figure, 
+plot_twoD_slices(twoD_slice_lst, number_of_files, grid_resolution_lst)
 
 % figure, 
 % hold on
@@ -54,7 +61,7 @@ function file_names = get_file_paths(directory)
     
     file_names = dir(directory);
     
-    mask = ismember({file_names.name}, {'.', '..'});
+    mask = ismember({file_names.name}, {'.', '..', '.DS_Store'});
     file_names(mask) = [];   %get rid of . and .. directories
     
     
@@ -84,9 +91,14 @@ function grid_resolution = get_grid_resolution(twoD_slice)
 
 end
 
-function summ = sum_output_datacube(twoD_slice, grid_resolution)
+function summ = sum_output_datacube(twoD_slice, bounds, grid_resolution)
     
-    summ = sum(sum(twoD_slice)) / grid_resolution^2; 
+
+    scaler = bounds^2 / grid_resolution^2; 
+    matrix = twoD_slice * scaler; 
+    summ = sum(sum(matrix)); 
+
+%     summ = sum(sum(twoD_slice)) / grid_resolution^2; 
     
 end
 
@@ -98,3 +110,23 @@ function line_profile = get_line_profile(twoD_slice, grid_resolution)
     line_profile = twoD_slice(:, half); 
 
 end 
+
+function plot_twoD_slices(twoD_slice_lst, number_of_files, grid_resolution_lst)
+
+        
+        figure, 
+        for i=1:number_of_files
+            
+            subplot(2, 3, i); 
+            logg = log10(twoD_slice_lst{i, 1}); 
+            imagesc(logg); 
+            caxis([1,7])% create and label the colorbar
+            cmap = jet();
+            colormap(cmap);
+            cb=colorbar; 
+            cb.Label.String = 'Log10 absorption density';
+            title(['resolution =', num2str(grid_resolution_lst(i))]);  
+                       
+        end
+      
+end
